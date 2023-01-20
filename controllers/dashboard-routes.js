22065c2f8fe7a26709d13e72c79db1476d8d2ef1
Brett Bates <brettbates86@gmail.com>
2023-01-20 16:07:26 -0600

    make it better

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../config/connnection');
+const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
